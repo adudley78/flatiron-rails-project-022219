3a707727a5cb9f92a56996eff5e2608ab7d3ae7e
adudley78 <dudleyadam@gmail.com>
2019-02-24 16:15:23 -0500

    Build basic index view

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
-  resources :todos
+
+  get 'site/index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  root 'site#index'
 end
