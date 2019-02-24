28debc1e30f56ecb92d382a9d714b01e04e23156
adudley78 <dudleyadam@gmail.com>
2019-02-24 17:36:18 -0500

    Implement projects#create in index view

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -2,12 +2,28 @@ class ProjectsController < ApplicationController
 
   # render all projects to projects/index.html.erb
   def index
+    @project = Project.new
     @projects = Project.all
     # raise @projects.inspect
   end
 
   def show
     @project = Project.find(params[:id])
+  end
+
+  def create
+    # raise params.inspect
+    @project = Project.create(project_params)
+    # @project.name = params[:project][:name]
+    # @project.save
+
+    redirect_to project_path(@project)
+  end
+
+  private
+
+  def project_params
+    params.require(:project).permit(:name)
   end
 
 end
