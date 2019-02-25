bc595fc8ba850c2f9912741f2359da8d94a4bfbc
adudley78 <dudleyadam@gmail.com>
2019-02-25 10:05:57 -0500

    Make issues display dynamically in projects#show view

--- app/models/issue.rb
+++ app/models/issue.rb
@@ -1,3 +1,4 @@
 class Issue < ApplicationRecord
+  # add association methods to thi model
   belongs_to :project
 end
--- app/models/project.rb
+++ app/models/project.rb
@@ -1,3 +1,4 @@
 class Project < ApplicationRecord
+  # add association methods to model
   has_many :issues
 end
