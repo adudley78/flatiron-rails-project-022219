149063cdaf43f908b908f5a1298891624d388246
adudley78 <dudleyadam@gmail.com>
2019-02-26 16:58:04 -0500

    Build issues#form_for_issue_status helper

--- app/helpers/issues_helper.rb
+++ app/helpers/issues_helper.rb
@@ -4,4 +4,17 @@ module IssuesHelper
     "completed" if issue.complete?
   end
 
+  def li_for_issue(issue)
+    content_tag_for :li, issue, :class => li_class_for_issue(issue) do
+      yield
+    end
+  end
+
+  # Render form_for inline within the method
+  def form_for_issue_status(issue)
+    form_for([issue.project, issue]) do |f|
+      f.check_box :status, :class => "toggle", :checked => issue.complete?
+    end
+  end
+
 end
