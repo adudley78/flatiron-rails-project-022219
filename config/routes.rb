7506c0b446d3bcf6743ed5fd3e7d0b1523428602
adudley78 <dudleyadam@gmail.com>
2019-03-01 18:38:16 -0500

    Built welcome#home

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,11 @@
 Rails.application.routes.draw do
 
+  get 'welcome/index'
   get '/login' => "sessions#new"
   post '/sessions' => "sessions#create"
   get '/logout' => "sessions#destroy"
   get '/auth/:github/callback', to: 'sessions#create'
+  get '/home' => "welcome#home"
 
   resources :users, :only => [:new, :create]
 
